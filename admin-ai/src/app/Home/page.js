c56413171a01e786656ea1901248f03ff5932276
brianpark888 <126150953+brianpark888@users.noreply.github.com>
2024-06-01 22:05:07 +0900

    changed the links to buttons

--- admin-ai/src/app/Home/page.js
+++ admin-ai/src/app/Home/page.js
@@ -1,8 +1,6 @@
 "use client";
 import Navbar from '@/Components/NavigationBar';
 import { useState, useEffect } from 'react';
-import Button from '@/Components/Button';
-import CreateForm from '@/Components/CreateForm';
 import FormList from '@/Components/FormList';
 import { useSession } from "next-auth/react";
 import { useRouter } from 'next/navigation'; // check if logged in
@@ -10,22 +8,12 @@ import { useRouter } from 'next/navigation'; // check if logged in
 export default function Home() {
     const { data: session, status } = useSession();
     const router = useRouter();
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
 
     useEffect(() => {
         if (status === "unauthenticated") {
             router.push('/Login');
         }
     }, [status, router]); // if not logged in, go to login page
-
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
 
     if (status === "loading") {
         return (
